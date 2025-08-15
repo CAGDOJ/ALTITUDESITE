09a84d827b1dee2fd62e7ff913d53e575e00b0c4
CAJOLV <OLIVEIRACAGOJ@GMAIL.COM>
2025-08-14 22:57:46 -0300

    Update supabaseClient.js

--- Projeto/4-java/supabaseClient.js
+++ Projeto/4-java/supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 // (API)
-const SUPABASE_URL  = "https://mxnvrxqwokvelulzdvmn.supabase.co";
+const SUPABASE_URL  = "https://www.portalaltitude.com.br/Projeto//1-html/index.html";
 const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14bnZyeHF3b2t2ZWx1bHpkdm1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NTQ4MjAsImV4cCI6MjA3MDQzMDgyMH0.DBntQQc91IWYAvMxHknJxjxxFAl5kiWOkc1LUXe_vKE";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON, {

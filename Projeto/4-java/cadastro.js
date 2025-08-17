636fa2aaad9d08498fbdcb956c32323477578af9
CAJOLV <OLIVEIRACAGOJ@GMAIL.COM>
2025-08-17 01:36:24 -0300

    Update cadastro.js

--- Projeto/4-java/cadastro.js
+++ Projeto/4-java/cadastro.js
@@ -210,3 +210,15 @@ form.addEventListener("submit", async (e)=>{
     toggleSubmit();
   }
 });
+
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const SUPABASE_URL = "https://mxnvrxqwokvelulzdvmn.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14bnZyeHF3b2t2ZWx1bHpkdm1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NTQ4MjAsImV4cCI6MjA3MDQzMDgyMH0.DBntQQc91IWYAvMxHknJxjxxFAl5kiWOkc1LUXe_vKE";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
+});
+
+// ✅ torna disponível para outros scripts que não estão importando ES modules
+window.supabase = supabase;

d53a58c99fc0614c43913492de85250c9c63fcc9
CAGOJ <OLIVEIRACAGOJ@GMAIL.COM>
2026-04-19 22:43:55 -0300

    Update supabase-init.js

--- Projeto/4-java/supabase-init.js
+++ Projeto/4-java/supabase-init.js
@@ -1,7 +1,7 @@
 // === Inicialização global do Supabase ===
 (function () {
-  const SUPABASE_URL = "https://mxnvrxqwokvelulzdvmn.supabase.co"; 
-  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14bnZyeHF3b2t2ZWx1bHpkdm1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NTQ4MjAsImV4cCI6MjA3MDQzMDgyMH0.DBntQQc91IWYAvMxHknJxjxxFAl5kiWOkc1LUXe_vKE"; // 👈 cola aqui a chave ANON completa
+  const SUPABASE_URL = "https://qwidlndoyhzvsrggwsba.supabase.co"; 
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3aWRsbmRveWh6dnNyZ2d3c2JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NDY4MjUsImV4cCI6MjA5MjIyMjgyNX0.CPcmbAC9KXLgdLVoAY_lRqjFyVzLLMiv38vKR2DFJeE"; // 👈 cola aqui a chave ANON completa
 
   // cria o cliente global
   window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
